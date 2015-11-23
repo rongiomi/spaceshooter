5b7fc7116ec8e41c1cb70e911ba174ee4be55149
children <children>
2015-11-22 19:20:27 -0800

    hopefully this is the base code part 2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public Boundary boundary;
+	
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate = 0.0000005F;
+	private float nextFire = 0.0F; 
+	
+	void Update ()
+		{
+		if (Input.GetButton("Fire1") && Time.time > nextFire) 
+			{
+			nextFire = Time.time + fireRate;
+			//  =
+			 Instantiate(shot, shotSpawn.position, shotSpawn.rotation);// as GameObject;
+			}
+		}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical); 
+		rigidbody.velocity = movement * speed;
+		
+		rigidbody.position = new Vector3 
+		(
+		Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax), 
+		0.0f, 
+		Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
+		);	
+	}
+}
